746429feed5b2a7d11a3f8fd87f9c9944b99dea9
uditrajmr3 <csense2653@gmail.com>
2025-01-12 15:32:28 +0530

    --updated: completed text expander

---
+++ src/Components/TextExpander.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+function TextExpander({
+  collapsedNumWords = 10,
+  expandButtonText = "Show more",
+  collapseButtonText = "Show less",
+  buttonColor = "#1ab1ff",
+  expanded = false,
+  className = "",
+  children,
+}) {
+  const [isExpanded, setIsExpanded] = useState(expanded);
+
+  function toggleExpansion() {
+    setIsExpanded((isExpanded) => !isExpanded);
+  }
+
+  function visibleContent() {
+    if (isExpanded) {
+      return children;
+    } else {
+      if (typeof children === "string") {
+        const words = children.split(" ");
+        const shrinked = words
+          .slice(0, collapsedNumWords)
+          .reduce((sentence, word) => sentence.concat(" ", word), "");
+        return shrinked;
+      }
+      return "Only Text can be expanded or shrinked. Please pass in a string";
+    }
+  }
+
+  return (
+    <p className={className}>
+      {visibleContent()}
+      {isExpanded ? "" : "..."}
+      <span onClick={toggleExpansion} style={{ color: buttonColor }}>
+        {isExpanded ? collapseButtonText : expandButtonText}
+      </span>
+    </p>
+  );
+}
+
+export default TextExpander;
